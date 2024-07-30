5c37b0973024f859abb63ad6348146eaeeb1a94d
nurcoda <mailo.mittelaedt@googlemail.com>
2024-07-30 15:20:29 +0200

    font and backgroundcolor

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { LandingpageComponent } from './landingpage/landingpage.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, LandingpageComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'Portfolio';
+  title = 'PortMailo';
 }
