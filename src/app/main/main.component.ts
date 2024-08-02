7d25f33ac89215a6ddc831654d187aabb18d56d9
nurcoda <mailo.mittelaedt@googlemail.com>
2024-08-02 10:13:00 +0200

    added portfolio img

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { LandingpageComponent } from './landingpage/landingpage.component';
 import { AboutmeComponent } from './aboutme/aboutme.component';
 import { MyskillComponent } from "./myskill/myskill.component";
+import { PortfolioComponent } from './portfolio/portfolio.component';
 
 @Component({
   selector: 'app-main',
   standalone: true,
-  imports: [LandingpageComponent, AboutmeComponent, MyskillComponent],
+  imports: [LandingpageComponent, AboutmeComponent, MyskillComponent, PortfolioComponent],
   templateUrl: './main.component.html',
   styleUrl: './main.component.scss'
 })
