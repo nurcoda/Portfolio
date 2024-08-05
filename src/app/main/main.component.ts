4b215170b25d3e55f7bff140ff337696373bc3dc
nurcoda <mailo.mittelaedt@googlemail.com>
2024-08-05 15:22:16 +0200

    many changes

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -3,12 +3,13 @@ import { LandingpageComponent } from './landingpage/landingpage.component';
 import { AboutmeComponent } from './aboutme/aboutme.component';
 import { MyskillComponent } from "./myskill/myskill.component";
 import { PortfolioComponent } from './portfolio/portfolio.component';
+import { ReferencesComponent } from './references/references.component';
 
 
 @Component({
   selector: 'app-main',
   standalone: true,
-  imports: [LandingpageComponent, AboutmeComponent, MyskillComponent, PortfolioComponent],
+  imports: [LandingpageComponent, AboutmeComponent, MyskillComponent, PortfolioComponent, ReferencesComponent],
   templateUrl: './main.component.html',
   styleUrl: './main.component.scss'
 })
