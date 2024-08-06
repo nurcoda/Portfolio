7bc91fb97cfc337c15ba025ded1864410f17e2d0
nurcoda <mailo.mittelaedt@googlemail.com>
2024-08-06 12:21:29 +0200

    added changes

--- src/app/main/portfolio/portfolio.component.ts
+++ src/app/main/portfolio/portfolio.component.ts
@@ -9,6 +9,21 @@ import { Component } from '@angular/core';
 })
 export class PortfolioComponent {
 
+  ngAfterViewInit() {
+    const slideElement = document.getElementById('slide');
 
+    if (slideElement) {
+      const observer = new IntersectionObserver((entries) => {
+        entries.forEach((entry) => {
+          if (entry.isIntersecting) {
+            slideElement.style.animation = 'slide 0.5s forwards';
+            observer.unobserve(slideElement); // Animation nur einmal ausführen
+          }
+        });
+      });
+
+      observer.observe(slideElement);
+    }
+  }
 
 }
