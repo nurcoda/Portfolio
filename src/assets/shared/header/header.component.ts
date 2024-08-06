c133da731c2b2eea9563cd653ec22b1015273ce5
nurcoda <mailo.mittelaedt@googlemail.com>
2024-08-06 16:29:22 +0200

    changes and nav fullscreen

--- src/assets/shared/header/header.component.ts
+++ src/assets/shared/header/header.component.ts
@@ -1,12 +1,22 @@
+import { NgClass } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [],
+  imports: [NgClass],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
 export class HeaderComponent {
+  isOpen: boolean = false;
+
+  toggleNav() {
+    if (this.isOpen) {
+      this.isOpen = false;
+    } else {
+      this.isOpen = true;
+    }
+  }
 
 }
