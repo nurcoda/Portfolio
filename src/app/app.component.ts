48c5c259a24d0d5d2c2b36b1b33ab4f6ce66bcbf
nurcoda <mailo.mittelaedt@googlemail.com>
2024-08-07 17:33:50 +0200

    update struktur

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,13 @@ import { RouterOutlet } from '@angular/router';
 import { LandingpageComponent } from './main/landingpage/landingpage.component';
 import { MainComponent } from './main/main.component';
 import { MyskillComponent } from './main/myskill/myskill.component';
+import { HeaderComponent } from "../assets/shared/header/header.component";
+import { FooterComponent } from "../assets/shared/footer/footer.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, LandingpageComponent, MainComponent, MyskillComponent],
+  imports: [RouterOutlet, LandingpageComponent, MainComponent, MyskillComponent, HeaderComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { HeaderComponent } from '../assets/shared/header/header.component';
+import { FooterComponent } from '../assets/shared/footer/footer.component';
+import { MainComponent } from './main/main.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+
+    { path: '', component: MainComponent },
+    // { path: 'imprint', component: imprintcon },
+
+];
