328c49f8ece354680e884a9e9ff18fc7783db04a
nurcoda <mailo.mittelaedt@googlemail.com>
2024-08-07 14:57:30 +0200

    switch addad

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -5,12 +5,13 @@ import { MyskillComponent } from "./myskill/myskill.component";
 import { PortfolioComponent } from './portfolio/portfolio.component';
 import { ReferencesComponent } from './references/references.component';
 import { FooterComponent } from '../../assets/shared/footer/footer.component';
+import { ContactComponent } from './contact/contact.component';
 
 
 @Component({
   selector: 'app-main',
   standalone: true,
-  imports: [LandingpageComponent, AboutmeComponent, MyskillComponent, PortfolioComponent, ReferencesComponent, FooterComponent],
+  imports: [LandingpageComponent, AboutmeComponent, MyskillComponent, PortfolioComponent, ReferencesComponent, FooterComponent, ContactComponent],
   templateUrl: './main.component.html',
   styleUrl: './main.component.scss'
 })
