a6014c4e9dde84efb9e906fe659bb59f1981e10b
nurcoda <mailo.mittelaedt@googlemail.com>
2024-08-08 11:17:23 +0200

    input field added

--- src/app/main/contact/contact.component.ts
+++ src/app/main/contact/contact.component.ts
@@ -1,13 +1,23 @@
 import { Component } from '@angular/core';
 import { FooterComponent } from '../../../assets/shared/footer/footer.component';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-contact',
   standalone: true,
-  imports: [FooterComponent],
+  imports: [FormsModule],
   templateUrl: './contact.component.html',
   styleUrl: './contact.component.scss'
 })
 export class ContactComponent {
 
+  contactData = {
+    name: "",
+    email: "",
+    message: "",
+  }
+
+  onSubmit() {
+    console.log(this.contactData)
+  }
 }
