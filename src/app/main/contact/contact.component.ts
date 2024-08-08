0a9a2410d337a49d90e9f059f987dd81d18a0ccd
nurcoda <mailo.mittelaedt@googlemail.com>
2024-08-08 12:27:07 +0200

    vaildation email

--- src/app/main/contact/contact.component.ts
+++ src/app/main/contact/contact.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
-import { FooterComponent } from '../../../assets/shared/footer/footer.component';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, NgForm } from '@angular/forms';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-contact',
@@ -11,13 +11,50 @@ import { FormsModule } from '@angular/forms';
 })
 export class ContactComponent {
 
+  http = inject(HttpClient);
+
   contactData = {
     name: "",
     email: "",
     message: "",
   }
 
-  onSubmit() {
-    console.log(this.contactData)
+  //   onSubmit(ngForm: NgForm) {
+  //     if (ngForm.valid && ngForm.submitted) {
+  //       console.log(this.contactData)
+  //     }
+  //   }
+
+  mailTest = true;
+
+  post = {
+    endPoint: 'https://deineDomain.de/sendMail.php',
+    body: (payload: any) => JSON.stringify(payload),
+    options: {
+      headers: {
+        'Content-Type': 'text/plain',
+        responseType: 'text',
+      },
+    },
+  };
+
+  onSubmit(ngForm: NgForm) {
+    if (ngForm.submitted && ngForm.form.valid && !this.mailTest) {
+      this.http.post(this.post.endPoint, this.post.body(this.contactData))
+        .subscribe({
+          next: (response) => {
+
+            ngForm.resetForm();
+          },
+          error: (error) => {
+            console.error(error);
+          },
+          complete: () => console.info('send post complete'),
+        });
+    } else if (ngForm.submitted && ngForm.form.valid && this.mailTest) {
+
+      ngForm.resetForm();
+    }
   }
+
 }
