d72601268f0be9e227ab08ed8239a173ab37278d
nurcoda <mailo.mittelaedt@googlemail.com>
2024-08-09 11:39:13 +0200

    new animation

--- src/app/main/portfolio/portfolio.component.ts
+++ src/app/main/portfolio/portfolio.component.ts
@@ -13,10 +13,12 @@ export class PortfolioComponent implements OnInit {
     window.addEventListener('scroll', this.onScroll);
   }
 
-  onScroll = () => {
-    const elements = document.querySelectorAll<HTMLElement>('.myskillarrow');
+  onScroll = (): void => {
+    const skillElements = document.querySelectorAll<HTMLElement>('.myskillarrow');
+    const projectElement = document.querySelector('.project-button-container') as HTMLElement;
 
-    elements.forEach((element) => {
+    // Animation für myskillarrow-Elemente
+    skillElements.forEach((element) => {
       const rect = element.getBoundingClientRect();
       const windowHeight = window.innerHeight || document.documentElement.clientHeight;
 
@@ -24,6 +26,55 @@ export class PortfolioComponent implements OnInit {
         element.classList.add('zoomInDown');
       }
     });
-  }
 
+    // Animation für project-button-container-Element
+    if (projectElement) {
+      const rect = projectElement.getBoundingClientRect();
+      const isVisible = rect.top >= 0 && rect.bottom <= window.innerHeight;
+
+      if (isVisible) {
+        projectElement.classList.add('slideInLeft');
+        // Optional: Entferne den Event-Listener, wenn die Animation nur einmal abgespielt werden soll
+        window.removeEventListener('scroll', this.onScroll);
+      }
+    }
+  }
 }
+
+
+// ngOnInit(): void {
+//   window.addEventListener('scroll', this.onScroll);
+// }
+
+// onScroll = () => {
+//   const elements = document.querySelectorAll<HTMLElement>('.myskillarrow');
+
+//   elements.forEach((element) => {
+//     const rect = element.getBoundingClientRect();
+//     const windowHeight = window.innerHeight || document.documentElement.clientHeight;
+
+//     if (rect.top <= windowHeight && rect.bottom >= 0) {
+//       element.classList.add('zoomInDown');
+//     }
+//   });
+// }
+
+// document.addEventListener('scroll', onScroll);
+
+// onScroll(): void {
+//   const element = document.querySelector('.project-button-container') as HTMLElement;
+
+//   if (element) {
+//     const rect = element.getBoundingClientRect();
+//     const isVisible = rect.top >= 0 && rect.bottom <= window.innerHeight;
+
+//     if (isVisible) {
+//       element.classList.add('slideInLeft');
+//       // Optional: Entferne den Event-Listener, wenn die Animation nur einmal abgespielt werden soll
+//       document.removeEventListener('scroll', onScroll);
+//     }
+//   }
+// }
+
+
+
