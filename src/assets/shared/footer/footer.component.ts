ebea94740820b2aabb763404e06f72bc616b01ae
nurcoda <mailo.mittelaedt@googlemail.com>
2024-08-12 18:48:47 +0200

    translate fin

--- src/assets/shared/footer/footer.component.ts
+++ src/assets/shared/footer/footer.component.ts
@@ -1,12 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { TranslationService } from '../../../app/translation.service';
+import { TranslateModule } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-footer',
   standalone: true,
-  imports: [],
+  imports: [TranslateModule],
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.scss'
 })
 export class FooterComponent {
-
+  translate = inject(TranslationService)
 }
