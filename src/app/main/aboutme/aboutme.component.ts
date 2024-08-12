8d28d0ebe38dcbc1ee3546e3f74d167499f00327
nurcoda <mailo.mittelaedt@googlemail.com>
2024-08-12 16:50:38 +0200

    header/aboutme translate fin

--- src/app/main/aboutme/aboutme.component.ts
+++ src/app/main/aboutme/aboutme.component.ts
@@ -1,12 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { TranslationService } from '../../translation.service';
+import { TranslateModule } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-aboutme',
   standalone: true,
-  imports: [],
+  imports: [TranslateModule],
   templateUrl: './aboutme.component.html',
   styleUrl: './aboutme.component.scss'
 })
 export class AboutmeComponent {
-
+  translate = inject(TranslationService)
 }
