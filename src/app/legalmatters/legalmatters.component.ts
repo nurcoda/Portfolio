9680706e00b3f88b4dd7be216abb0eeab6affc07
nurcoda <mailo.mittelaedt@googlemail.com>
2024-08-13 09:14:04 +0200

    added legalmatters and button change

---
+++ src/app/legalmatters/legalmatters.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-legalmatters',
+  standalone: true,
+  imports: [],
+  templateUrl: './legalmatters.component.html',
+  styleUrl: './legalmatters.component.scss'
+})
+export class LegalmattersComponent {
+
+}
